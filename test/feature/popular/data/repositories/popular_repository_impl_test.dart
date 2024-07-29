53db5e6b8fdbecfdc371ec29b7b3733ef57832ee
cem256 <cemo99@gmail.com>
2024-07-29 23:16:10 +0300

    fix failing test cases and add additional tests for popular and search repositories #24

--- test/feature/popular/data/repositories/popular_repository_impl_test.dart
+++ test/feature/popular/data/repositories/popular_repository_impl_test.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
+import 'package:manga_clean_arch/app/errors/exceptions/exceptions.dart';
 import 'package:manga_clean_arch/app/errors/failures/failures.dart';
 import 'package:manga_clean_arch/feature/popular/data/datasources/remote/popular_remote_datasource.dart';
 import 'package:manga_clean_arch/feature/popular/data/models/manga/manga_model.dart';
@@ -10,12 +12,10 @@ import 'package:mocktail/mocktail.dart';
 
 class MockPopularRemoteDataSource extends Mock implements PopularRemoteDataSource {}
 
-class MockMangaModel extends Mock implements MangaModel {}
-
 void main() {
   late MockPopularRemoteDataSource dataSource;
   late PopularRepository repository;
-  late List<MockMangaModel> mockModel;
+  late List<MangaModel> mockModel;
   late int page;
   late int limit;
 
@@ -24,24 +24,67 @@ void main() {
     limit = 20;
     dataSource = MockPopularRemoteDataSource();
     repository = PopularRepositoryImpl(dataSource: dataSource);
-    mockModel = List.generate(limit, (_) => MockMangaModel());
+    mockModel = List.generate(
+      limit,
+      (_) => const MangaModel(
+        title: 'Berserk',
+      ),
+    );
   });
 
   test('Returns Right with List of MangaEntities when there is no exception', () async {
     when(() => dataSource.getPopularMangas(page: page, limit: limit)).thenAnswer((_) async => mockModel);
     final result = await repository.getPopularMangas(page: page, limit: limit);
 
-    verify(() => dataSource.getPopularMangas(page: page, limit: limit));
+    verify(() => dataSource.getPopularMangas(page: page, limit: limit)).called(1);
 
     expect(result, isA<Right<Failure, List<MangaEntity>>>());
+    result.fold(
+      (failure) => fail('Expected Right but got Left with $failure'),
+      (mangas) {
+        expect(mangas, isA<List<MangaEntity>>());
+        expect(mangas.length, mockModel.length);
+      },
+    );
   });
 
-  test('Returns Left with Failure type when an exception caught', () async {
-    when(() => dataSource.getPopularMangas(page: page, limit: limit)).thenThrow(Exception());
+  test('Returns Left with NetworkFailure when DioException is thrown', () async {
+    when(() => dataSource.getPopularMangas(page: page, limit: limit))
+        .thenThrow(DioException(requestOptions: RequestOptions()));
     final result = await repository.getPopularMangas(page: page, limit: limit);
 
-    verify(() => dataSource.getPopularMangas(page: page, limit: limit));
+    verify(() => dataSource.getPopularMangas(page: page, limit: limit)).called(1);
 
     expect(result, isA<Left<Failure, List<MangaEntity>>>());
+    result.fold(
+      (failure) => expect(failure, isA<NetworkFailure>()),
+      (mangas) => fail('Expected Left but got Right with $mangas'),
+    );
+  });
+
+  test('Returns Left with NullResponseFailure when NullResponseException is thrown', () async {
+    when(() => dataSource.getPopularMangas(page: page, limit: limit)).thenThrow(NullResponseException());
+    final result = await repository.getPopularMangas(page: page, limit: limit);
+
+    verify(() => dataSource.getPopularMangas(page: page, limit: limit)).called(1);
+
+    expect(result, isA<Left<Failure, List<MangaEntity>>>());
+    result.fold(
+      (failure) => expect(failure, isA<NullResponseFailure>()),
+      (mangas) => fail('Expected Left but got Right with $mangas'),
+    );
+  });
+
+  test('Returns Left with UnknownFailure when an unknown exception is thrown', () async {
+    when(() => dataSource.getPopularMangas(page: page, limit: limit)).thenThrow(Exception('Unknown error'));
+    final result = await repository.getPopularMangas(page: page, limit: limit);
+
+    verify(() => dataSource.getPopularMangas(page: page, limit: limit)).called(1);
+
+    expect(result, isA<Left<Failure, List<MangaEntity>>>());
+    result.fold(
+      (failure) => expect(failure, isA<UnknownFailure>()),
+      (mangas) => fail('Expected Left but got Right with $mangas'),
+    );
   });
 }
